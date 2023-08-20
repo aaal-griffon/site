7c712c039b1f894094500f20a77658b72614cf52
Efe Kerem EFEKAN <131147466+WingenTer@users.noreply.github.com>
2023-08-21 02:26:13 +0300

    Responsive Menu and some bugs solved

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const mainpage = document.querySelectorAll("#mainpage");
     const iletisim = document.querySelectorAll("#iletisim");
     const SSS = document.querySelectorAll("#SSS");
+//mobile menus values
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const body = document.querySelector('body2');
+
+
+
+
     basvurLinks.forEach(function (link) {
       link.addEventListener("click", function (event) {
         event.preventDefault(); // Öntanımlı davranışı engelle
   
         // Yeni bir siteye yönlendir
-        window.location.href = "kayit";
+        window.location.href = "kayit/";
       });
     });
     ekipler.forEach(function (link) {
@@ -19,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault(); // Öntanımlı davranışı engelle
   
         // Yeni bir siteye yönlendir
-        window.location.href = "ekipler";
+        window.location.href = "ekipler/";
       });
     });
     mainpage.forEach(function (link) {
@@ -27,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault(); // Öntanımlı davranışı engelle
   
         // Yeni bir siteye yönlendir
-        window.location.href = "index.html";
+        window.location.href = "";
       });
     });
     iletisim.forEach(function (link) {
@@ -35,7 +43,7 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault(); // Öntanımlı davranışı engelle
   
         // Yeni bir siteye yönlendir
-        window.location.href = "iletisim";
+        window.location.href = "iletisim/";
       });
     });
     SSS.forEach(function (link) {
@@ -43,7 +51,62 @@ document.addEventListener("DOMContentLoaded", function () {
         event.preventDefault(); // Öntanımlı davranışı engelle
   
         // Yeni bir siteye yönlendir
-        window.location.href = "SSS";
+        window.location.href = "SSS/";
       });
   });
+  function adjustElementSizes() {
+    const windowHeight = window.innerHeight;
+
+    // Hero bölüm boyutu ayarla
+    const hero = document.querySelector('.hero');
+    hero.style.height = windowHeight * 0.3 + 'px';
+
+    // Vizyon ve Misyon kutularının boyutlarını ayarla ve içeriklerini büyüt
+    const visionMissionBoxes = document.querySelectorAll('.vision, .mission');
+    visionMissionBoxes.forEach(box => {
+        const targetHeight = windowHeight * 0.3; // Ekran yüksekliğinin %60'ı
+        box.style.height = targetHeight + 'px';
+        
+        // Kutu içindeki yazıları büyüt
+        const textElements = box.querySelectorAll('h2, p');
+        textElements.forEach(textElement => {
+            const fontSize = targetHeight * 0.08; // Örnek: Kutu yüksekliğinin %8'i
+            textElement.style.fontSize = fontSize + 'px';
+        });
+    });
+//mobile menu codes
+
+mobileMenuButton.addEventListener('click', () => {
+  // Mobil menüyü göster veya gizle
+  if (mobileMenu.style.display === 'block') {
+      mobileMenu.style.display = 'none';
+  } else {
+      mobileMenu.style.display = 'block';
+  }
+});
+  }
+  body.addEventListener('click', () => {
+    // Mobil menüyü göster veya gizle
+    if (mobileMenu.style.display === 'block') {
+        mobileMenu.style.display = 'none';
+    }
+  });
+
+function setMobileMenuStyle() {
+  const header = document.querySelector('header');
+  mobileMenu.style.width = header.offsetWidth + 'px'; // Header genişliğine ayarlayın
+  mobileMenu.style.backgroundColor = getComputedStyle(header).backgroundColor; // Header arkaplan rengine ayarlayın
+}
+function placeMobileMenuBelowHeader() {
+  const header = document.querySelector('header');
+  const headerHeight = header.clientHeight;
+
+  mobileMenu.style.top = headerHeight + 'px';
+}
+window.addEventListener('DOMContentLoaded', setMobileMenuStyle);
+window.addEventListener('resize', setMobileMenuStyle);
+window.addEventListener('DOMContentLoaded', placeMobileMenuBelowHeader);
+window.addEventListener('resize', placeMobileMenuBelowHeader);
+window.addEventListener('resize', adjustElementSizes);
+window.addEventListener('load', adjustElementSizes);
   });
